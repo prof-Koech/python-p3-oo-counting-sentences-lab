9fa92e81e943fed53d4111640ed8ef20f9268f3d
Kennedy <kiplangatkennedy019@gmail.com>
2023-05-22 09:44:50 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+      def __init__(self, value=''):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            print("The value must be a string.")
+
+
+      def is_sentence(self):
+        return self.value.endswith('.')
+
+      def is_question(self):
+        return self.value.endswith('?')
+
+      def is_exclamation(self):
+        return self.value.endswith('!')
+
+      def count_sentences(self):
+        sentences = [sentence.strip() for sentence in self.value.split('.') if sentence.strip()]
+        return len(sentences)
+    
+string = MyString("Hello World. How Wonderful is it to be in this great great world!?")
